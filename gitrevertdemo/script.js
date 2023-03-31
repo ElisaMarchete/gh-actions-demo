b59153939ee202adc19247e15c4956724326bfe9
Joao Marchete <elisamarchete@gmail.com>
2023-03-30 20:50:32 -0400

    inicial commit

---
+++ gitrevertdemo/script.js
@@ -0,0 +1,25 @@
+$(function () {
+  var skillNames = [
+    'Bootstrap',
+    'C',
+    'C++',
+    'CSS',
+    'Express.js',
+    'Git',
+    'HTML',
+    'Java',
+    'JavaScript',
+    'jQuery',
+    'JSON',
+    'MySQL',
+    'Node.js',
+    'NoSQL',
+    'PHP',
+    'Python',
+    'React',
+    'Ruby',
+  ];
+  $('#skill-name').autocomplete({
+    source: skillNames,
+  });
+});
